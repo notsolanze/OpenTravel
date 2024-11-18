bd94f13483764d50f0ada386493680dbdbfd2011
notsolanze <spm.lesterlancee@gmail.com>
2024-11-18 11:54:34 +0800

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'opentravel-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  'https://cdn.tailwindcss.com',
+  'https://unpkg.com/leaflet@1.7.1/dist/leaflet.css',
+  'https://unpkg.com/leaflet@1.7.1/dist/leaflet.js',
+  'https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap',
+  'https://unpkg.com/lucide@latest',
+  'https://cdn-icons-png.flaticon.com/128/10473/10473293.png',
+  'https://assets.mixkit.co/active_storage/sfx/2568/2568-preview.mp3'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
